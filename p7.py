b1d7f9bd7ec992cf7ef1bf4a2683cdd6e2cd1cb5
rashidand123 <rashidang12@gmail.com>
2026-02-25 13:59:41 +0000

    p7,py

---
+++ p7.py
@@ -0,0 +1,6 @@
+#perform pythagorus theoram 
+#take input from the user the base and perependicular from the user and display the hypotenuse
+b=int(input("enter b "))
+p=int(input("enter p "))
+hypo=(b**2+p**2)**1/2
+print(hypo)
