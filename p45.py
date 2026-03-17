7b845f158e17a52aee31ef075c73fc69f32156de
Rashi Dang <rashidang12@gmail.com>
2026-03-17 17:48:24 +0000

    p45.py

---
+++ p45.py
@@ -0,0 +1,9 @@
+#string
+str1="this is a string.we are creating it in python"
+print(str1)
+
+str1="this is a string.\n we are creating it in python"
+print(str1)
+
+str1="this is a string.\t we are creating it in python"
+print(str1)
