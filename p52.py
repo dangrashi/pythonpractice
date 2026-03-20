323c23ca228fc3d42e820be4c3c7a460bd5ee572
Rashi Dang <rashidang12@gmail.com>
2026-03-20 18:23:30 +0000

    p52.py

---
+++ p52.py
@@ -0,0 +1,18 @@
+#string functions
+#replace function 
+str="i am studying"
+print(str.replace("i","a"))
+
+#find
+str="i am studying"
+print(str.find("t"))
+
+str="i am studying"
+print(str.find("q"))
+
+#count
+str="i am from studying from "
+print(str.count("from"))
+
+str=" i am from studying from "
+print(str.count("o"))
