1cbead02ccd3efec49c831de1e6971871f7420c9
Rashi Dang <rashidang12@gmail.com>
2026-03-22 22:26:38 +0530

    Add conditional statements for traffic light simulation

---
+++ p56.py
@@ -0,0 +1,13 @@
+#conditional statements 
+light="green"
+
+if(light == "red"):
+  print("stop")
+
+elif(light == "green"):
+  print("go")
+
+elif(light == "yellow"):
+  print("look")
+
+print("end of code")  
