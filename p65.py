ee5d06b01dade64979527cb63d5f41eb55bb3183
Rashi Dang <rashidang12@gmail.com>
2026-03-26 15:10:58 +0000

    p65.py

---
+++ p65.py
@@ -0,0 +1,32 @@
+#list methods 
+list=[1,2,3]
+list.append(4)
+print(list)
+
+list=[10, 13, 8, 6]
+list.sort()
+print(list)
+
+list=[10,20,32,44,38]
+list.sort(reverse=True)
+print(list)
+
+list=["a", "d", "h", "b"]
+list.sort()
+print(list)
+
+list=["a", "d", "h", "b"]
+list.reverse()
+print(list)
+
+list=[2,1,3]
+list.insert(1,5)
+print(list)
+
+list=[2,1,3,2]
+list.pop(2)
+print(list)
+
+list=[2,1,3,2]
+list.remove(2)
+print(list)
