a42b3ae32a687ee3b1af6d6c8aa815e4abb4f82d
Rashi Dang <rashidang12@gmail.com>
2026-04-03 09:17:25 +0000

    p81.py

---
+++ p81.py
@@ -0,0 +1,18 @@
+#store following word meanings in a python dictionary .
+#table: "a piece of furniture","lists of facts and figures"
+#cat: "a small animal"
+
+dictionary={
+    "cat" : " a small animal",
+    "table" : ["a piece of furniture","lists of facts and figures"]
+}
+print(dictionary)
+
+#you are given a list of subjects for students. assume one classroom is required for 1 
+#subject. How many classrooms are needed by all students . 
+#"python","java","C++","python","javascript","java","python","java","C++","C"
+
+subjects = {
+    "python", "java","C++","python","javascript","java","python","java","C++","C"
+}
+print(len(subjects))
