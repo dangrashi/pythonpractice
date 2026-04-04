8b115e9393fae81fec56ff2712d979687d7310c0
Rashi Dang <rashidang12@gmail.com>
2026-04-04 22:30:19 +0530

    p84.py

---
+++ p84.py
@@ -0,0 +1,32 @@
+#print numbers from 1 to 100
+i = 1 
+while i <= 100:
+  print(i)
+  i += 1
+
+#print numbers from 100 to 1 
+i = 100 
+while i>=1:
+  print(i)
+  i -= 1 
+
+#print the multiplication table of a number n .
+i=1 
+while i <= 10:
+  print(3*i)
+  i +=1 
+
+
+n= int(input("enter a number:"))
+i=1 
+while i<= 10:
+  print(n*i)
+  i += 1
+
+#print the elements of the following list using a loop:
+nums= [1,4,9,16,25,36,49,64,81,100]
+
+i=0
+while i < len(nums):
+  print(nums[i])
+  i+=1
