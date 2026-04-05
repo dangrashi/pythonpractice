cd211254db5700b569b637d14fe58ea6e5617413
Rashi Dang <rashidang12@gmail.com>
2026-04-05 15:03:38 +0000

    P86.PY

---
+++ p86.py
@@ -0,0 +1,20 @@
+#for loop 
+nums = [ 1,2,3,4,5]
+
+for val in nums:
+    print (val)
+
+tup =(1,2,3,4,5)
+
+for num in tup:
+    print(num)
+
+str=("hello")
+
+for char in str:
+    if(char == "o"):
+        print("o found")
+        break
+    print(char)
+
+print("END")
