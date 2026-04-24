0e1a88dff82fb4dba69a672e4c3b85274a1cb857
Rashi Dang <rashidang12@gmail.com>
2026-04-24 22:17:54 +0530

    p126.py

---
+++ p126.py
@@ -0,0 +1,5 @@
+Accept two numbers from the user and display their sum
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+print(f"The sum is: {num1 + num2}")
