d0353bf0a95be6451f8c83b1117f936be7ab6159
Rashi Dang <rashidang12@gmail.com>
2026-05-11 18:16:18 +0000

    p164.py

---
+++ p164.py
@@ -0,0 +1,5 @@
+#Given prices = [45, 22, 89, 12, 56], find the highest and lowest values without using a loop
+
+prices = [45, 22, 89, 12, 56]
+print("Max:", max(prices))
+print("Min:", min(prices))
