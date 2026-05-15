cb7af719c1915978882b773e27191986fe3f1e6b
Rashi Dang <rashidang12@gmail.com>
2026-05-15 19:26:51 +0000

    p172.py

---
+++ p172.py
@@ -0,0 +1,8 @@
+# Write a snippet to check if the key "price" exists in the car dictionary. If not, print "Key not found"
+
+car = {"brand": "Toyota", "model": "Camry"}
+
+if "price" in car:
+    print(car["price"])
+else:
+    print("Key not found")
